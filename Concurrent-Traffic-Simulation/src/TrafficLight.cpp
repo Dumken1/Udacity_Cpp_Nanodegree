2884c1e442708479d4b700d341ef5b9635829c60
chibundu <chibunduogbonnia@gmail.com>
2021-03-14 16:37:19 +0100

    Initial stage of project

---
+++ Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <random>
+#include <thread>
+#include <future>
+#include "TrafficLight.h"
+
+/* Implementation of class "MessageQueue" */
+
+ 
+template <typename T>
+T MessageQueue<T>::receive()
+{
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+
+    // remove last vector element from queue
+    T q_state = std::move(_queue.back());
+     _queue.clear();
+
+    return q_state;
+}
+
+template <typename T>
+void MessageQueue<T>::send(T &&msg)
+{
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
+}
+
+/* Implementation of class "TrafficLight" */
+
+ 
+TrafficLight::TrafficLight()
+{
+    _currentPhase = TrafficLightPhase::red;
+}
+
+void TrafficLight::waitForGreen()
+{
+    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // runs and repeatedly calls the receive function on the message queue. 
+    // Once it receives TrafficLightPhase::green, the method returns.
+    while(true){
+        if(_message.receive() == TrafficLightPhase::green){
+            return;
+        }
+    }
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase()
+{
+    return _currentPhase;
+}
+
+void TrafficLight::simulate()
+{
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+}
+
+// virtual function which is executed in a thread
+void TrafficLight::cycleThroughPhases()
+{
+    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
+    // and toggles the current phase of the traffic light between red and green and sends an update method 
+    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+     // Random value between 4 and 6 for cycle duration.
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<int> t_range(4, 6);
+    long cycleDuration = t_range(eng);
+  
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    bool toggle = false;
+    
+    lastUpdate = std::chrono::system_clock::now();
+    while(true){
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); //reduce CPU speed
+
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if (timeSinceLastUpdate >= cycleDuration){
+            toggle ^= 1;
+            switch (toggle){
+                case true:
+                    _currentPhase = TrafficLightPhase::green;
+                    break;
+                default:
+                    _currentPhase = TrafficLightPhase::red;
+                    break;
+             }
+
+             auto sent_phase = std::async(std::launch::async, &MessageQueue<TrafficLightPhase>::send, &_message, std::move(_currentPhase));
+             sent_phase.wait();
+
+
+            lastUpdate = std::chrono::system_clock::now();
+            cycleDuration = t_range(eng);
+        }
+      
+     }
+}
